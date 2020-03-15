56dab0d087a2b5692077e6a0a9c7c5cbd1a92348
Waseem Ahmed <codewaseem@gmail.com>
2020-03-15 15:11:57 +0530

    disable observer for ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,5 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+import { useStaticRendering } from "mobx-react-lite";
+useStaticRendering();
+
 import wrapWithProvider from "./rootWrapper";
 export const wrapRootElement = wrapWithProvider;
