4e9dd01cd180a137dfa5ea3a69898c5e6903a932
Waseem Ahmed <codewaseem@gmail.com>
2020-03-15 07:01:23 +0530

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Index = () => {
+  return (
+    <div>
+      <h1>
+        Hello, world! adsfnalksdfj klasdjfalksd fasdfladflakjdf alsdkf jaldfjldd
+        adflakj asfjlaksdf alakdsfjjk
+      </h1>
+    </div>
+  );
+};
+
+export default Index;
