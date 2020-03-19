874a1cae95d1742dac189a112a8347880f0dbda2
Waseem Ahmed <codewaseem@gmail.com>
2020-03-19 14:28:07 +0530

    refactor

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,76 +2,96 @@ import React from "react";
 // eslint-disable-next-line no-unused-vars
 import { css } from "@emotion/core";
 import useStores from "../hooks/useStores";
+import styled from "@emotion/styled";
 
 interface headerProps extends React.AllHTMLAttributes<any> {}
 
-export const PureHeader: React.FC<headerProps> = ({ ...props }) => {
-  let store = useStores();
+const Header: React.FC<headerProps> = ({ ...props }) => {
   return (
-    <header
-      css={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: 30,
-      }}
-      {...props}
-    >
-      <div>
-        <img src="/images/logo-bookmark.svg" alt="Welcome to Bookmark It" />
-      </div>
-      <nav>
-        <h1 className="sr-only">Navigation</h1>
-        <span
-          css={css`
-            @media screen and (min-width: 850px) {
-              display: none;
-            }
-          `}
-        >
-          <img src="/images/icon-hamburger.svg" alt="Open Navigation" />
-        </span>
-        <ul
-          css={css`
-            list-style: none;
-            > * {
-              padding: 10px 35px;
-              flex: 1;
-              cursor: pointer;
-              text-transform: uppercase;
-
-              :hover {
-                color: ${store.theme.colors.softRed};
-              }
-            }
-            display: none;
-            @media screen and (min-width: 850px) {
-              display: flex;
-            }
-          `}
-        >
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Contact</li>
-          <li
-            css={css`
-              background: ${store.theme.colors.softRed};
-              color: white;
-              border-radius: 5px;
-              box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
-              border: 1px solid ${store.theme.colors.softRed};
-
-              :hover {
-                background: white;
-              }
-            `}
-          >
-            Login
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <StyledHeader {...props}>
+      <Logo />
+      <Navigation />
+    </StyledHeader>
   );
 };
 
-export default PureHeader;
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30;
+`;
+
+const Logo = () => (
+  <div>
+    <img src="/images/logo-bookmark.svg" alt="Welcome to Bookmark It" />
+  </div>
+);
+
+const Navigation = () => (
+  <nav>
+    <h1 className="sr-only">Navigation</h1>
+    <MobileNav />
+    <DesktopNav />
+  </nav>
+);
+
+const MobileNav = () => {
+  return (
+    <span
+      css={css`
+        @media screen and (min-width: 850px) {
+          display: none;
+        }
+      `}
+    >
+      <img src="/images/icon-hamburger.svg" alt="Open Navigation" />
+    </span>
+  );
+};
+
+const DesktopNav = () => {
+  let store = useStores();
+  return (
+    <ul
+      css={css`
+        list-style: none;
+        > * {
+          padding: 10px 35px;
+          flex: 1;
+          cursor: pointer;
+          text-transform: uppercase;
+
+          :hover {
+            color: ${store.theme.colors.softRed};
+          }
+        }
+        display: none;
+        @media screen and (min-width: 850px) {
+          display: flex;
+        }
+      `}
+    >
+      <li>Features</li>
+      <li>Pricing</li>
+      <li>Contact</li>
+      <li
+        css={css`
+          background: ${store.theme.colors.softRed};
+          color: white;
+          border-radius: 5px;
+          box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+          border: 1px solid ${store.theme.colors.softRed};
+
+          :hover {
+            background: white;
+          }
+        `}
+      >
+        Login
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
