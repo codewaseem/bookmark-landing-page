015c40223791327cfa248de671e231f6f8985d64
Waseem Ahmed <codewaseem@gmail.com>
2020-03-19 14:20:06 +0530

    responsive nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
 import { css } from "@emotion/core";
+import useStores from "../hooks/useStores";
 
 interface headerProps extends React.AllHTMLAttributes<any> {}
 
 export const PureHeader: React.FC<headerProps> = ({ ...props }) => {
+  let store = useStores();
   return (
     <header
       css={{
@@ -20,9 +22,53 @@ export const PureHeader: React.FC<headerProps> = ({ ...props }) => {
       </div>
       <nav>
         <h1 className="sr-only">Navigation</h1>
-        <span>
+        <span
+          css={css`
+            @media screen and (min-width: 850px) {
+              display: none;
+            }
+          `}
+        >
           <img src="/images/icon-hamburger.svg" alt="Open Navigation" />
         </span>
+        <ul
+          css={css`
+            list-style: none;
+            > * {
+              padding: 10px 35px;
+              flex: 1;
+              cursor: pointer;
+              text-transform: uppercase;
+
+              :hover {
+                color: ${store.theme.colors.softRed};
+              }
+            }
+            display: none;
+            @media screen and (min-width: 850px) {
+              display: flex;
+            }
+          `}
+        >
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Contact</li>
+          <li
+            css={css`
+              background: ${store.theme.colors.softRed};
+              color: white;
+              border-radius: 5px;
+              box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+              border: 1px solid ${store.theme.colors.softRed};
+
+              :hover {
+                background: white;
+              }
+            `}
+          >
+            Login
+          </li>
+        </ul>
       </nav>
     </header>
   );
